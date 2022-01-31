fdf424f98725ff4b1329698f0fe9179e2b5c085e
Cusatelli <einarvandevelde@gmail.com>
2022-01-31 08:49:58 +0100

    feat: added boilerplate Profile.js

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+export default function Profile({ auth }) {
+  if (!auth) { return <Navigate to="/login" />; }
+
+  return (
+    <div className="page profile">
+      <h1>Profile Page</h1>
+    </div>
+  );
+}
