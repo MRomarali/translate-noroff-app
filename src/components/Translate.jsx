2785d533478bf61eb57618f59732c72ae55d14f6
Cusatelli <einarvandevelde@gmail.com>
2022-01-31 08:50:08 +0100

    feat: added boilerplate Translate.js

---
+++ src/components/Translate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+export default function Translate({ auth }) {
+  if (!auth) { return <Navigate to="/login" />; }
+
+  return (
+    <div className="page translate">
+      <h1>Translate Page</h1>
+    </div>
+  );
+}
