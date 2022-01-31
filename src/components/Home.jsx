ff33c1f2cf21c2dfeb0bee4c46c9da04b39a26f2
Cusatelli <einarvandevelde@gmail.com>
2022-01-31 08:49:36 +0100

    feat: added boilerplate Home.js

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="page home">
+      <div className="header">
+        <img src="./logo.png" alt="Header" className="foreground" />
+        <img src="./header.jpg" alt="Header" className="background" />
+      </div>
+    </div>
+  );
+}
