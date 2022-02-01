2f9fff7ecaba590c587037267cd73087688dd921
Cusatelli <einarvandevelde@gmail.com>
2022-02-01 18:33:00 +0100

    refactor(Server): revert to previous state.

--- server.js
+++ server.js
@@ -1,58 +1,42 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// // const router = jsonServer.router('db.json');
-// const path = require('path');
-// const router = jsonServer.router(path.join(__dirname, 'db.json'));
-// const middlewares = jsonServer.defaults({
-//   static: './build'
-// });
-
-// const PORT = process.env.PORT || 3000;
-
-// // Set default middlewares (logger, static, cors and no-cache)
-// server.use(middlewares);
-
-// // To handle POST, PUT and PATCH you need to use a body-parser
-// // You can use the one used by JSON Server
-// server.use(jsonServer.bodyParser);
-
-// server.use((req, _res, next) => {
-//   if (req.method === 'POST') {
-//     req.body.createdAt = Date.now();
-//   }
-//   // Continue to JSON Server router
-//   next();
-// });
-
-// // server.use(jsonServer.rewriter({
-// //   '/api/*': '/$1',
-// // }));
-
-// // Use default router 
-// server.use('/api', router);
-// server.listen(PORT, () => {
-//   console.log(
-//     '__________________________________\n\n'
-//     + '    JSON Server is running at:\n\n'
-//     + `\u001b[34m----> http://localhost:${PORT} <----\u001b[0m`
-//     + '\n__________________________________\n'
-//   );
-// });
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('db.json');
+// const path = require('path');
+// const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults({
   static: './build'
 });
+
 const PORT = process.env.PORT || 4000;
 
+// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}));
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
 
-server.use(router);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router 
+server.use('/api', router);
 server.listen(PORT, () => {
-  console.log('Server is running');
+  console.log(
+    '\n__________________________________\n\n'
+    + ' JSON Server is running!\n\n'
+    + ' Heroku App:\n'
+    + `\u001b[35m https://translate-noroff-app.herokuapp.com/ \u001b[0m\n\n`
+    + ' Localhost:\n'
+    + `\u001b[34m http://localhost:${PORT} \u001b[0m\n\n`
+    + ' Resources:\n'
+    + ` API:   \u001b[33m http://localhost:${PORT}/api \u001b[0m\n`
+    + ` Users: \u001b[33m http://localhost:${PORT}/api/users \u001b[0m`
+    + '\n__________________________________\n'
+  );
 });
