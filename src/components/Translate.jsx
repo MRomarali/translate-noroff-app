5b124b24746a054271636149b334df6477148250
Cusatelli <einarvandevelde@gmail.com>
2022-02-01 15:26:51 +0100

    fix(Reducer): replace all useState with useReducer.

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,22 +1,33 @@
 import React, { useReducer } from "react";
 import { Navigate } from "react-router-dom";
-import { useState } from "react/cjs/react.development";
 import { apiPostTranslationsRequest } from "../api/Index";
 import { getAuth, setSessionTranslations } from "../storage/Session";
 
 export default function Translate() {
   const initialState = {
-    value: ""
+    value: "",
+    sequence: [],
+    disabled: false,
   };
 
   const reducer = (state, action) => {
+    console.log(action)
     switch (action.type) {
       case "INPUT":
-        console.log(action)
         return {
           ...state,
           value: action
         };
+      case "DISABLE":
+        return {
+          ...state,
+          disabled: action
+        };
+      case "SEQUENCE":
+        return {
+          ...state,
+          sequence: action
+        }
       default:
         return state;
     }
@@ -24,28 +35,34 @@ export default function Translate() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
   // const [value, setValue] = useState(() => { return ""; });
-  const [imageSequence, setImageSequence] = useState(() => { return []; });
-  const [disabled, setDisabled] = useState(() => { return false; });
+  // const [imageSequence, setImageSequence] = useState(() => { return []; });
+  // const [disabled, setDisabled] = useState(() => { return false; });
 
   const auth = getAuth();
   if (!auth) { return <Navigate to="/Login" />; }
 
   const handleChange = (event) => {
-    setDisabled(false);
+    // setDisabled(false);
+    dispatch({
+      disabled: false,
+      type: 'DISABLE'
+    });
     const regExp = /[^A-Za-z\s]/g;
     dispatch({
-      state: event.target.value.replaceAll(regExp, ""),
+      value: event.target.value.replaceAll(regExp, ""),
       type: 'INPUT'
     }); // Trim input from invalid characters
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setDisabled(true);
+    // setDisabled(true);
+    dispatch({
+      disabled: true,
+      type: 'DISABLE'
+    });
     const regExp = /[^A-Za-z]/g;
-    console.log(state)
-    let input = state.value.state;
-    input = input.replaceAll(regExp, "");
+    const input = state.value.value.replaceAll(regExp, "");
     if (input < 1) {
       alert('Please enter a word before submitting.');
       return;
@@ -56,28 +73,33 @@ export default function Translate() {
       tempArray.push(input[index]);
     }
 
-    setImageSequence(tempArray);
+    // setImageSequence(tempArray);
+    dispatch({
+      sequence: tempArray,
+      type: 'SEQUENCE'
+    });
     apiPostTranslationsRequest(getAuth().id, input, tempArray);
     setSessionTranslations(input, tempArray);
   }
 
   return (
     <div className="page translate">
-      <form onSubmit={handleSubmit}>
+      <form value={state.value.value} onSubmit={handleSubmit}>
         <label>English</label>
-        <input value={state.state} onChange={handleChange} placeholder="Hello..." />
+        <input onChange={handleChange} placeholder="Hello..." />
         <label>American Sign-Language</label>
         <div>
-          {imageSequence.map((char, index) => {
+          {/*{imageSequence.map((char, index) => {*/}
+          {state.sequence.sequence ? state.sequence.sequence.map((char, index) => {
             return <img
               src={`American_SignLanguage_A-Z/${char.toUpperCase()}.png`}
               alt={char.toUpperCase()}
               key={index}
               className="sign-language"
             />
-          })}
+          }) : console.log(state.sequence.sequence)}
         </div>
-        <button type="submit" disabled={disabled} className={disabled ? "disabled" : ""}>Send</button>
+        <button type="submit" disabled={state.disabled.disabled} className={state.disabled.disabled ? "disabled" : ""}>Send</button>
       </form>
     </div>
   );
