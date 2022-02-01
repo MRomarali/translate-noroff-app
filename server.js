ac1e36cb355ddfe83ce00e4fb348258a8632d9c8
Cusatelli <einarvandevelde@gmail.com>
2022-02-01 19:05:33 +0100

    chore: absolute router path.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-// const path = require('path');
-// const router = jsonServer.router(path.join(__dirname, 'db.json'));
+// const router = jsonServer.router('db.json');
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults({
   static: './build'
 });
